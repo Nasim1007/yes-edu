d624ac5cab1b5090cfddf4cf90a40db12b3068aa
Nasim1007 <nasimi.iskandar@mail.ru>
2022-12-05 09:19:54 +0000

    addaadsdas

--- components/UI/CourseCard/CourseCard.tsx
+++ components/UI/CourseCard/CourseCard.tsx
@@ -8,7 +8,7 @@ import { useRouter } from 'next/router'
 
 interface CourseCardProps extends ICourse {
   className?: string
-  link: any
+  link: boolean
 }
 
 function CourseCard({name, id, icon, className, info, student, link}: CourseCardProps) {
