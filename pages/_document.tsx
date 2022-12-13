81851232562f477d7036cbda11e6c492a01c4519
Nasim Rahmatov <109575501+Nasim1007@users.noreply.github.com>
2022-12-13 14:26:33 +0500

    -----

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,6 +9,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700&display=swap"
           rel="stylesheet"/>
           <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+      <meta name="google-site-verification" content="SwLaCbTiIPUCkuYeQtUhioN4cLDA2lnIOz20dRTgVBI" />
           <meta property="og:title" content="Образовательный цент Yes-Edu."/>
           <meta property="og:description" content="Образовательный цент Yes-Edu , это центр где используется  инновационная методика  которая даёт настоящий результат, что на шаг приближает к мечте."/>
           <meta property="og:image" content="/assets/android-chrome-192x192.png"/>
@@ -19,7 +20,7 @@ export default function Document() {
       />
       <Script id="google-analytics" strategy="afterInteractive">
         {`
-            window.dataLayer = window.dataLayer  [];
+            window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-BCJ0LP0ZRC');
